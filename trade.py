a8b1b6dc72093cf124ab68329bfe4ea9af824267
Lee Grey <lee.grey@henderex.com>
2019-07-15 23:27:31 -0400

    Exits now working but only for the first Trade object

--- trade.py
+++ trade.py
@@ -15,22 +15,27 @@ class Trade:
         self.targetPrice = -1
         self.stopPrice = -1
         self.market = create(market)
+        self.monitor = self.market.pipe(
+            op.filter(lambda v: v['symbol'] == self.ticker),
+            #op.do_action(print),
+            op.subscribe_on(pool_scheduler)
+        )
         self.entryTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
-            op.do_action(lambda x: print(f'entryTrigger {x}')),
             op.filter(lambda q: self.position == 0 and q['ask'] >= self.entryPrice),
+            op.do_action(lambda x: print(f'entryTrigger {x}')),
             op.subscribe_on(pool_scheduler)
         )
         self.targetTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
-            op.do_action(lambda x: print(f'targetTrigger {x}')),
             op.filter(lambda q: self.position > 0 and q['bid'] >= self.targetPrice),
+            op.do_action(lambda x: print(f'targetTrigger {x}')),
             op.subscribe_on(pool_scheduler)
         )
         self.stopTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
-            op.do_action(lambda x: print(f'stopTrigger {x}')),
             op.filter(lambda q: self.position > 0 and q['bid'] <= self.stopPrice),
+            op.do_action(lambda x: print(f'stopTrigger {x}')),
             op.subscribe_on(pool_scheduler)
         )
         self.subscriptions = {}
@@ -42,15 +47,15 @@ class Trade:
         self.targetPrice = target
         self.stopPrice = stop
 
+        self.subscriptions['monitor'] = self.monitor.subscribe(
+            lambda x: print(x),
+            scheduler=pool_scheduler
+        )
+
         self.subscriptions['entryTrigger'] = self.entryTrigger.subscribe(
             lambda x: self.openPosition(),
             scheduler=pool_scheduler
         )
-
-
-    def openPosition(self):
-        print(f'bought {self.quantity} {self.ticker}')
-        self.position = self.quantity
 
         self.subscriptions['targetTrigger'] = self.targetTrigger.subscribe(
             lambda x: self.closePosition(),
@@ -62,6 +67,11 @@ class Trade:
             scheduler=pool_scheduler
         )
 
+
+    def openPosition(self):
+        print(f'bought {self.quantity} {self.ticker}')
+        self.position = self.quantity
+
     def closePosition(self):
         print(f'sold {self.quantity} {self.ticker}')
         self.position = 0
@@ -69,12 +79,15 @@ class Trade:
 
     def dispose(self):
         print('dispose')
-        if 'entryTrigger' in self.subscriptions:
+        if 'entryTrigger' in self.subscriptions and self.position > 0:
             self.subscriptions['entryTrigger'].dispose()
             del self.subscriptions['entryTrigger']
-        if 'targetTrigger' in self.subscriptions:
+        if 'targetTrigger' in self.subscriptions and self.position == 0:
             self.subscriptions['targetTrigger'].dispose()
             del self.subscriptions['targetTrigger']
-        if 'stopTrigger' in self.subscriptions:
+        if 'stopTrigger' in self.subscriptions and self.position == 0:
             self.subscriptions['stopTrigger'].dispose()
             del self.subscriptions['stopTrigger']
+        if 'monitor' in self.subscriptions:
+            self.subscriptions['monitor'].dispose()
+            del self.subscriptions['monitor']
