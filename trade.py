f2212b2667c89b4a0e3e253c9345d4e21e5e84f6
Lee Grey <lee.grey@henderex.com>
2019-07-15 22:54:46 -0400

    Tried to add exits as well

--- trade.py
+++ trade.py
@@ -14,13 +14,27 @@ class Trade:
         self.entryPrice = -1
         self.targetPrice = -1
         self.stopPrice = -1
-        self.quotesource = create(market).pipe(
+        self.market = create(market)
+        self.entryTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
-            op.do_action(print),
+            op.do_action(lambda x: print(f'entryTrigger {x}')),
             op.filter(lambda q: self.position == 0 and q['ask'] >= self.entryPrice),
             op.subscribe_on(pool_scheduler)
         )
-        self.subscription = None
+        self.targetTrigger = self.market.pipe(
+            op.filter(lambda v: v['symbol'] == self.ticker),
+            op.do_action(lambda x: print(f'targetTrigger {x}')),
+            op.filter(lambda q: self.position > 0 and q['bid'] >= self.targetPrice),
+            op.subscribe_on(pool_scheduler)
+        )
+        self.stopTrigger = self.market.pipe(
+            op.filter(lambda v: v['symbol'] == self.ticker),
+            op.do_action(lambda x: print(f'stopTrigger {x}')),
+            op.filter(lambda q: self.position > 0 and q['bid'] <= self.stopPrice),
+            op.subscribe_on(pool_scheduler)
+        )
+        self.subscriptions = {}
+
 
     def setupTrade(self, qty, entry, target, stop):
         self.quantity = qty
@@ -28,7 +42,7 @@ class Trade:
         self.targetPrice = target
         self.stopPrice = stop
 
-        self.subscription = self.quotesource.subscribe(
+        self.subscriptions['entryTrigger'] = self.entryTrigger.subscribe(
             lambda x: self.openPosition(),
             scheduler=pool_scheduler
         )
@@ -38,8 +52,29 @@ class Trade:
         print(f'bought {self.quantity} {self.ticker}')
         self.position = self.quantity
 
+        self.subscriptions['targetTrigger'] = self.targetTrigger.subscribe(
+            lambda x: self.closePosition(),
+            scheduler=pool_scheduler
+        )
+
+        self.subscriptions['stopTrigger'] = self.stopTrigger.subscribe(
+            lambda x: self.closePosition(),
+            scheduler=pool_scheduler
+        )
+
+    def closePosition(self):
+        print(f'sold {self.quantity} {self.ticker}')
+        self.position = 0
+
 
     def dispose(self):
-        if self.subscription:
-            self.subscription.dispose()
-            self.subscription = None
+        print('dispose')
+        if 'entryTrigger' in self.subscriptions:
+            self.subscriptions['entryTrigger'].dispose()
+            del self.subscriptions['entryTrigger']
+        if 'targetTrigger' in self.subscriptions:
+            self.subscriptions['targetTrigger'].dispose()
+            del self.subscriptions['targetTrigger']
+        if 'stopTrigger' in self.subscriptions:
+            self.subscriptions['stopTrigger'].dispose()
+            del self.subscriptions['stopTrigger']
