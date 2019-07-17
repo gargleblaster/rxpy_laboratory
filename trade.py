e1da832bac7a16e135dcea12cecc742b9f1385f8
Lee Grey <lee.grey@henderex.com>
2019-07-16 21:53:55 -0400

    Incorporate Alex's logging with thread name

--- trade.py
+++ trade.py
@@ -1,10 +1,13 @@
 import rx
 from rx import of, create, operators as op
-from rx.concurrency import ThreadPoolScheduler
+from rx.scheduler import ThreadPoolScheduler
 import multiprocessing
+import logging
 
 thread_count = multiprocessing.cpu_count() + 1
 pool_scheduler = ThreadPoolScheduler(thread_count)
+
+logging.basicConfig(level=logging.INFO, format="[%(threadName)-23s] %(asctime)-15s %(message)s ")
 
 class Trade:
     def __init__(self, ticker, market):
@@ -23,19 +26,19 @@ class Trade:
         self.entryTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
             op.filter(lambda q: self.position == 0 and q['ask'] >= self.entryPrice),
-            op.do_action(lambda x: print(f'entryTrigger {x}')),
+            op.do_action(lambda x: logging.info(f'entryTrigger {x}')),
             op.subscribe_on(pool_scheduler)
         )
         self.targetTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
             op.filter(lambda q: self.position > 0 and q['bid'] >= self.targetPrice),
-            op.do_action(lambda x: print(f'targetTrigger {x}')),
+            op.do_action(lambda x: logging.info(f'targetTrigger {x}')),
             op.subscribe_on(pool_scheduler)
         )
         self.stopTrigger = self.market.pipe(
             op.filter(lambda v: v['symbol'] == self.ticker),
             op.filter(lambda q: self.position > 0 and q['bid'] <= self.stopPrice),
-            op.do_action(lambda x: print(f'stopTrigger {x}')),
+            op.do_action(lambda x: logging.info(f'stopTrigger {x}')),
             op.subscribe_on(pool_scheduler)
         )
         self.subscriptions = {}
@@ -48,7 +51,7 @@ class Trade:
         self.stopPrice = stop
 
         self.subscriptions['monitor'] = self.monitor.subscribe(
-            lambda x: print(x),
+            lambda x: logging.info(x),
             scheduler=pool_scheduler
         )
 
@@ -69,16 +72,16 @@ class Trade:
 
 
     def openPosition(self):
-        print(f'bought {self.quantity} {self.ticker}')
+        logging.info(f'bought {self.quantity} {self.ticker}')
         self.position = self.quantity
 
     def closePosition(self):
-        print(f'sold {self.quantity} {self.ticker}')
+        logging.info(f'sold {self.quantity} {self.ticker}')
         self.position = 0
 
 
     def dispose(self):
-        print('dispose')
+        logging.info('dispose')
         if 'entryTrigger' in self.subscriptions and self.position > 0:
             self.subscriptions['entryTrigger'].dispose()
             del self.subscriptions['entryTrigger']
