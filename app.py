45671423f6f3cdcfbc4bed54e39f6e478177d957
Yevgeny Menaker <jeka.menaker@gmail.com>
2019-07-29 13:03:42 -0700

    Grouped observable and logging updates

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from stock import Stock
 
 def main():
     config.quoteObserver.requestQuotes('AAPL,AMZN,GOOGL,FB,NFLX')
+    '''
     config.stocks['AAPL'] = Stock(config.quoteObserver, 'AAPL')
     config.stocks['AMZN'] = Stock(config.quoteObserver, 'AMZN')
     config.stocks['GOOGL'] = Stock(config.quoteObserver, 'GOOGL')
     config.stocks['FB'] = Stock(config.quoteObserver, 'FB')
     config.stocks['NFLX'] = Stock(config.quoteObserver, 'NFLX')
+    '''
 
 if __name__ == '__main__':
     main()
